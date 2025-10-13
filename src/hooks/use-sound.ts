e1c91adb6ae074aaa099b147bf871df94b059a3f
Enzo Gimena <enzogimena.shawn@gmail.com>
2025-10-13 08:18:27 +0000

    Try fixing this error: `Runtime Error: Error: Failed to execute 'createB

--- src/hooks/use-sound.ts
+++ src/hooks/use-sound.ts
@@ -7,17 +7,17 @@ export const useSound = () => {
 
   const playClick = () => {
     // A simple click sound
-    zzfx.apply(null, [1.05, , 224, 0.02, 0.02, 0.08, 1, 1.72, -13.9, , , , 0.19, , 0.2, , 0.43, 0.02]);
+    zzfx(...[1.05, , 224, 0.02, 0.02, 0.08, 1, 1.72, -13.9, , , , 0.19, , 0.2, , 0.43, 0.02]);
   };
   
   const playGenerate = () => {
     // A success/generate sound
-    zzfx.apply(null, [1.5, , 650, .05, .2, .3, 4, 1.5, , , 580, .02, .02, , , , .08, .5, .1]);
+    zzfx(...[1.5, , 650, .05, .2, .3, 4, 1.5, , , 580, .02, .02, , , , .08, .5, .1]);
   };
 
   const playError = () => {
     // An error sound
-    zzfx.apply(null, [1.5, ,349,.04,.13,.29,2,.1,-9.5,3.2,,,,,,.1,.6,.02,.33]);
+    zzfx(...[1.5, ,349,.04,.13,.29,2,.1,-9.5,3.2,,,,,,.1,.6,.02,.33]);
   }
 
   // You can add more sounds here
