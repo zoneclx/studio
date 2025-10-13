9196756874e99af59fbd063be9ceee9120e740b0
Enzo Gimena <enzogimena.shawn@gmail.com>
2025-10-13 09:10:24 +0000

    On ultra dark mode create its own unique design on the mode switcher

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,7 +1,8 @@
+
 'use client';
 
 import * as React from 'react';
-import { Moon, Sun } from 'lucide-react';
+import { Moon, Sun, Eclipse } from 'lucide-react';
 import { useTheme } from '@/context/theme-context';
 
 import { Button } from '@/components/ui/button';
@@ -20,7 +21,8 @@ export function ThemeToggle() {
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 ultra:-rotate-90 ultra:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 ultra:rotate-0 ultra:scale-100" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 ultra:rotate-90 ultra:scale-0" />
+          <Eclipse className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:scale-0 ultra:rotate-0 ultra:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
