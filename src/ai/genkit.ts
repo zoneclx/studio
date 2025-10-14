afb3b4fdddbfc38d945fa9d7f7103926ec3eff0e
Enzo Gimena <enzogimena.shawn@gmail.com>
2025-10-14 03:28:08 +0000

    same bro lemme use my api

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({ apiVersion: 'v1' })],
+  plugins: [googleAI({ apiKey: process.env.GEMINI_API_KEY, apiVersion: 'v1' })],
   model: 'googleai/gemini-2.5-flash',
 });
