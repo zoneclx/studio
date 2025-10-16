5a16700fd059f731001fc8d9347c166cac3e8e2d
Enzo Gimena <enzogimena.shawn@gmail.com>
2025-10-16 11:37:26 +0000

    Change to footer to © Enzo-Gimena, All rights reserved.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { cn } from '@/lib/utils';
@@ -8,7 +7,7 @@ export default function Footer() {
     <footer className={cn("py-4 text-center text-sm z-10 text-muted-foreground bg-background/80 backdrop-blur-lg border-t")}>
       <div className="container">
         <p>
-          © 2025 Mono Studio, All rights reserved.
+          © Enzo-Gimena, All rights reserved.
         </p>
       </div>
     </footer>
